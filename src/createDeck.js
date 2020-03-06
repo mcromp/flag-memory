9be9ee66fd6752958a8338aa6bec61c14a793e68
Matthew Crompton <MLCrompton@matt-mb-air.localdomain>
2020-03-06 15:41:09 +0100

    Clean up logic createDeck

--- src/createDeck.js
+++ src/createDeck.js
@@ -1,20 +1,14 @@
 import fetchWiki from "./fetchWiki";
 
-export default function createDeck(
-  setCardDeck,
-  countryData,
-  setCountryData,
-  deckSizeNum
-) {
-  let countries = [...countryData];
-  shuffle(countries);
+const createDeck = (setCardDeck, countryData, setCountryData, deckSizeNum) => {
+  let countries = shuffle([...countryData]);
   countries = countries.slice(0, deckSizeNum);
-  countries.map(c => {
-    return !countryData[c.index].wiki ? fetchWiki(c, setCountryData) : c;
-  });
-  shuffle((countries = [...countries, ...countries]));
+  countries.map(c =>
+    !countryData[c.index].wiki ? fetchWiki(c, setCountryData) : c
+  );
+  countries = shuffle([...countries, ...countries]);
   setCardDeck(countries.map((c, i) => ({ ...c, boardIndex: i })));
-}
+};
 
 //Fisher–Yates shuffle function
 const shuffle = array => {
@@ -22,4 +16,7 @@ const shuffle = array => {
     let j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
+  return array;
 };
+
+export default createDeck;
