4c3c9aa62dedd29926117544b84f3a5b3cce10a9
Matthew Crompton <MLCrompton@matt-mb-air.localdomain>
2020-03-06 16:50:14 +0100

    Separate Wikiboard component

--- src/WikiBoard.js
+++ src/WikiBoard.js
@@ -1,18 +1,22 @@
 import React from "react";
 
-function WikiBoard({ wikiBoardData, countryData }) {
-  let board = wikiBoardData.map((country, i) => {
+const WikiBoard = ({ wikiBoardData, countryData }) => {
+  let board = wikiBoardData.map(country => {
     let { name, flag, wiki } = countryData[country];
-    return (
-      <div key={i} className="wiki">
-        <h1>{name}</h1>
-        <img alt={`flag of ${name}`} src={flag} />
-        <p>{wiki}</p>
-      </div>
-    );
+    return <WikiCountryCard name={name} flag={flag} wiki={wiki} key={name} />;
   });
 
   return <div className="wikiBoard">{board}</div>;
-}
+};
+
+const WikiCountryCard = ({ name, flag, wiki }) => {
+  return (
+    <div key={name} className="wiki">
+      <h1>{name}</h1>
+      <img alt={`flag of ${name}`} src={flag} />
+      <p>{wiki}</p>
+    </div>
+  );
+};
 
 export default WikiBoard;
